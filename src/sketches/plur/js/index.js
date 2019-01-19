66f1f8dc9e0757fa5522140d0aecd638c39e9571
Tyler Paige <tylerclarkpaige@gmail.com>
2019-01-19 14:33:26 -0500

    adds content to plur

--- src/sketches/plur/js/index.js
+++ src/sketches/plur/js/index.js
@@ -2,9 +2,11 @@ require('../scss/styles.scss');
 import { roundTo } from './util.js';
 
 const init = () => {
+  const container = document.querySelector('.grid');
+  const containerBBox = container.getBoundingClientRect();
   const letters = document.querySelectorAll('.letter');
   const letterBBox = letters[0].getBoundingClientRect();
-  const scaleY = roundTo(window.innerHeight / letterBBox.height, 2);
+  const scaleY = roundTo(containerBBox.height / letterBBox.height, 2);
   [...letters].forEach((letter) => {
     letter.style.transform = `scaleY(${scaleY})`;
   });
